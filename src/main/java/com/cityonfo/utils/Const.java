72b5da6bb6ad6fd394093f5841784204a60a5f86
andrewXYP <andrew_xyp@qq.com>
2017-05-26 14:48:02 +0800

    第一次提交  修改

---
+++ src/main/java/com/cityonfo/utils/Const.java
@@ -0,0 +1,19 @@
+package com.cityonfo.utils;
+
+
+public class Const {
+	
+	public static final String SESSION_SECURITY_CODE = "sessionSecCode";
+	public static final String SESSION_USER = "sessionUser";
+	public static final String PAGE = "info/config/PAGE.txt"; // 分页条数配置路径
+	public static final String EMAIL = "info/config/EMAIL.txt";		//邮箱服务器配置路径
+	public static final String FILEREALPATHIMG = "D:/www1/huajingxi-0b44449e56d474ac0156e4f045b205c6/dmkelectronics/ROOT/uploadFiles/uploadImgs/";// 图片产品
+	public static final String PRODUCTPATHIMG = "D:/www1/huajingxi-0b44449e56d474ac0156e4f045b205c6/dmkelectronics/ROOT/uploadFiles/productImgs/";// 客户设计稿
+	public static final String FILEPATHIMG = "/uploadFiles/uploadImgs/"; // 图片上传路径
+	public static final String PRODUCTPATH = "/uploadFiles/productImgs/"; // 图片上传路径
+	public static final String FILEPATHFILE = "/uploadFiles/file/";		//文件上传路径
+	public static final String STATICIMG = "D:/www1/huajingxi-0b44449e56d474ac0156e4f045b205c6/dmkelectronics/ROOT/public/image/";
+	public static final String NO_INTERCEPTOR_PATH = ".*/.*";
+
+	
+}
---
+++ src/main/java/com/cityonfo/utils/DateUtil.java
@@ -0,0 +1,87 @@
+package com.cityonfo.utils;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateUtil {
+	private final static SimpleDateFormat sdfYear = new SimpleDateFormat("yyyy");
+
+	private final static SimpleDateFormat sdfDay = new SimpleDateFormat(
+			"yyyy-MM-dd");
+	
+	private final static SimpleDateFormat sdfDays = new SimpleDateFormat(
+	"yyyyMMdd");
+
+	private final static SimpleDateFormat sdfTime = new SimpleDateFormat(
+			"yyyy-MM-dd HH:mm:ss");
+
+	/**
+	 * 获取YYYY格式
+	 * 
+	 * @return
+	 */
+	public static String getYear() {
+		return sdfYear.format(new Date());
+	}
+
+	/**
+	 * 获取YYYY-MM-DD格式
+	 * 
+	 * @return
+	 */
+	public static String getDay() {
+		return sdfDay.format(new Date());
+	}
+	
+	/**
+	 * 获取YYYYMMDD格式
+	 * 
+	 * @return
+	 */
+	public static String getDays(){
+		return sdfDays.format(new Date());
+	}
+
+	/**
+	 * 获取YYYY-MM-DD HH:mm:ss格式
+	 * 
+	 * @return
+	 */
+	public static String getTime() {
+		return sdfTime.format(new Date());
+	}
+
+	/**
+	* @Title: compareDate
+	* @Description: TODO(日期比较，如果s>=e 返回true 否则返回false)
+	* @param s
+	* @param e
+	* @return boolean  
+	* @throws
+	* @author luguosui
+	 */
+	public static boolean compareDate(String s, String e) {
+		if(fomatDate(s)==null||fomatDate(e)==null){
+			return false;
+		}
+		return fomatDate(s).getTime() >=fomatDate(e).getTime();
+	}
+
+	/**
+	 * 格式化日期
+	 * 
+	 * @return
+	 */
+	public static Date fomatDate(String date) {
+		DateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
+		try {
+			return fmt.parse(date);
+		} catch (ParseException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
